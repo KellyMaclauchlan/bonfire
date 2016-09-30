66de3393fab844f56616476ab04b06dfcee06a5d
Kelly Maclauchlan <kellymaclauchlan@kellys-macbook-pro.local>
2016-09-30 12:11:31 -0400

    Initial Commit

---
+++ Bonfire/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Bonfire
+//
+//  Created by Kelly Maclauchlan on 2016-09-30.
+//  Copyright © 2016 Kelly Maclauchlan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
